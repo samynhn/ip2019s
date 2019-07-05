16bfd4150610cd8bf1eea6a3e4df3ccf262aa34e
shan5383 <52521390+shan5383@users.noreply.github.com>
2019-07-05 17:28:21 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -5,6 +5,38 @@
 
 function start(e) {
 
+var name = [
+{
+    char:'單',
+    big5:'B3E6',
+    cns:'1-5E4C',
+    unicode:'55AE'
+},
+{
+    char:'綿',
+    big5:'BAF8',
+    cns:'1-6A41',
+    unicode:'7DBF'
+},
+{
+    char:'恆',
+    big5:'ABED',
+    cns:'1-5131',
+    unicode:'6046'
+}
+];
+
+var ele =document.getElementById("div1");
+for (var i=0;i<3;i++) {
+    ele.innerHTML +=name[i].char;
+    ele.innerHTML += 'big5 =' ;
+    ele.innerHTML +=name[i].big5;
+    ele.innerHTML += 'cns =' ;
+    ele.innerHTML +=name[i].cns;
+    ele.innerHTML += 'unicode =' ;
+    ele.innerHTML +=name[i].unicode;
+    ele.innerHTML +='<br>';
+}
 }
 
 /*
